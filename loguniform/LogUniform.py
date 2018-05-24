4558195d326cc85480d18126bba1c26ae4b1a224
João Faria <joao.faria@astro.up.pt>
2018-05-24 15:18:02 +0100

    create package

--- loguniform/LogUniform.py
+++ loguniform/LogUniform.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy import stats
 from abc import ABC, abstractmethod
 
 # the "Jeffreys" or log-uniform prior is already implemented in scipy
@@ -28,26 +27,94 @@ class dist(ABC):
         return self.a <= x, x <= self.b
 
     @abstractmethod
-    def pdf(self, x): pass
+    def pdf(self, x): 
+        """ Probability density function evaluated at x. """
+        pass
 
     @abstractmethod
-    def cdf(self, x): pass
+    def cdf(self, x): 
+        """ Cumulative density function evaluated at x. """
+        pass
 
     @abstractmethod
-    def ppf(self, x): pass
+    def ppf(self, x): 
+        """ Percent point function (inverse of `cdf`) evaluated at q. """
+        pass
 
     def sf(self, x):
+        """ Survival function (1 - `cdf`) evaluated at x. """
         return 1 - self.cdf(x)
 
     def rvs(self, size=1):
-        r = np.random.uniform(size=size)
-        return self.ppf(r)
+        """
+        Random samples from the distribution.
+
+        Parameters
+        ----------
+        size : int or tuple of ints, optional
+            Number of random samples to draw (default is 1).
+
+        Returns
+        -------
+        rvs : ndarray or scalar
+        """
+        u = np.random.uniform(size=size)
+        s = self.ppf(u)
+        return np.asscalar(s) if size==1 else s
+
+    def interval(self, alpha):
+        """
+        Confidence interval with equal areas around the median.
+
+        Parameters
+        ----------
+        alpha : array_like of float
+            Probability that an rv will be drawn from the returned range.
+            Each value should be in the range [0, 1].
+
+        Returns
+        -------
+        a, b : ndarray of float
+            end-points of range that contains 100 * alpha % of the
+            distribution support.
+        """
+
+        alpha = np.asarray(alpha)
+        if np.any((alpha > 1) | (alpha < 0)):
+            raise ValueError("alpha must be between 0 and 1 inclusive")
+        q1 = (1.0-alpha)/2
+        q2 = (1.0+alpha)/2
+        a = self.ppf(q1)
+        b = self.ppf(q2)
+        return a, b
 
 
 class LogUniform(dist):
+    """ 
+    A log-uniform distribution, sometimes called "reciprocal" or, in some
+    contexts, used as a "Jeffreys prior".
+
+    Parameters
+    ----------
+    a, b : float
+           Lower and upper bounds of the distribution.
+    
+    Methods
+    -------
+    pdf    : probability density function
+    logpdf : logarithm of the probability density function
+    cdf    : cumulative density function
+    ppf    : percent point function (inverse of cdf)
+
+    Properties
+    ----------
+    mean, mode, var, std, skewness, kurtosis
+
+    """
+
     # defaults for lower and upper bounds of the distribution
-    a = 1
-    b = 100
+    a = None
+    b = None
 
     def __init__(self, a, b):
         assert a>0 and b>0, \
@@ -68,7 +135,7 @@ class LogUniform(dist):
         x = np.asarray(x)
         m = self._support_mask(x)
         with np.errstate(divide='ignore'):
-            return np.where(m, -np.log(x*np.log(self.q)), 0.)
+            return np.where(m, -np.log(x*np.log(self.q)), -np.inf)
 
     def cdf(self, x):
         x = np.asarray(x)
@@ -78,23 +145,30 @@ class LogUniform(dist):
     def ppf(self, p):
         return np.exp(np.log(self.a) + p*(self.q))
 
-
     @property
     def mean(self):
         return (self.b - self.a) / self.q
+    
     @property
     def mode(self):
         return self.a
+    
     @property
     def var(self):
         a, b, q = self.a, self.b, self.q
         return (b-a)*(b*(q-2) + a*(q+2)) / (2*q**2)
+    
+    @property
+    def std(self):
+        return np.sqrt(self.var)
+    
     @property
     def skewness(self):
         a, b, q = self.a, self.b, self.q
         f1 = np.sqrt(2) * (12*q*(a-b)**2 + q**2*(b**2*(2*q-9) + 2*a*b*q + a**2*(2*q+9)))
         f2 = 3*q*np.sqrt(b-a)*(b*(q-2)+a*(q+2))**(3/2)
         return f1/f2
+    
     @property
     def kurtosis(self):
         # see wikipedia.org/wiki/Kurtosis to understand the -3
@@ -108,6 +182,30 @@ class LogUniform(dist):
 
 
 class ModifiedLogUniform(dist):
+    """ 
+    A modified log-uniform distribution, sometimes called a "modified Jeffreys prior".
+    The support of the distribution includes zero.
+
+    Parameters
+    ----------
+    knee : float
+           Point where the distribution changes from uniform to log-uniform.
+    b    : float
+           Upper bound of the distribution.
+    
+    Methods
+    -------
+    pdf    : probability density function
+    logpdf : logarithm of the probability density function
+    cdf    : cumulative density function
+    ppf    : percent point function (inverse of cdf)
+
+    Properties
+    ----------
+    mean, mode, var, std, skewness, kurtosis
+
+    """
+
     # defaults for lower and upper bounds and knee
     a = 0
     b = 100
@@ -128,6 +226,12 @@ class ModifiedLogUniform(dist):
         m = self._support_mask(x)
         with np.errstate(divide='ignore'):
             return np.where(m, 1/( (x+self.knee) * self.q ), 0.)
+
+    def logpdf(self, x):
+        x = np.asarray(x)
+        m = self._support_mask(x)
+        with np.errstate(divide='ignore'):
+            return np.where(m, -np.log(x+self.knee) - np.log(self.q), -np.inf)
 
     def cdf(self, x):
         r = self.b / self.knee
@@ -154,6 +258,10 @@ class ModifiedLogUniform(dist):
                 + 2*knee**2*np.log(knee+b) \
                 + (4*knee*np.log(knee+b) - 4*knee*np.log(knee)-4*b)*mu \
                 + (2*np.log(knee+b)-2*np.log(knee))*mu**2)/(2*q)
+    
+    @property
+    def std(self):
+        return np.sqrt(self.var)
 
     @property
     def skewness(self):
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='LogUniform',
+      version='1.0',
+      description='Implementation of the log-uniform and modified log-uniform distributions',
+      author='João Faria',
+      author_email='joao.faria@astro.up.pt',
+      url='https://github.com/j-faria/LogUniform',
+      packages=['loguniform'],
+     )
